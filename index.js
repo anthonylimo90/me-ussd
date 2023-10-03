a8fa1dcc275ebae6c6d0887fa0165cf504a5b75e
Anthony Kiplimo <anthonylimo90@gmail.com>
2023-10-03 15:57:25 +0300

    Updated host and port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT;
 
 app.use(express.json());
 
