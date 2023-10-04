ecdea3929fc340231e35e2a193320066b648b20a
Anthony Kiplimo <anthonylimo90@gmail.com>
2023-10-04 11:24:52 +0300

    Updated to work with new APIs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const axios = require("axios");
 const express = require("express");
 const app = express();
 
@@ -10,6 +11,22 @@ app.use(express.urlencoded({extended: false}));
 app.get("/", (req, res) => {
     console.log(`Hey hey 😊`);
 });
+
+const generateRandomNumber = () => {
+    let randomNumber = Math.floor(Math.random * 20);
+    let randomNumberString = randomNumber.toString();
+    return randomNumberString;
+};
+
+const getCurrentDate = () => {
+    let date = new Date();
+    return date.toISOString().substring(0, 10);
+};
+
+const removeThePlus = (phoneNumber) => {
+    let correctPhoneNumber = phoneNumber.substring(1);
+    return correctPhoneNumber;
+};
 
 app.post("/ussd", (req, res) => {
 
@@ -25,9 +42,58 @@ app.post("/ussd", (req, res) => {
     let response = "";
 
     if(text == "") {
-        response = `CON What would you like to check?
-        1. Account Balance
-        2. KYC Status`
+        // To show this menu a customer has to be registred
+        // Perform a customer query check before proceeding
+        // If not registered, show a menu asking the customer to register
+
+        const data = axios.post(
+            "https://sandbox.loop.co.ke/v1/customer/query",
+            {
+                "requestDateTime": getCurrentDate(),
+                "requestId": generateRandomNumber(),
+                "userIdType": "P",
+                "reserve1": "",
+                "reserve2": "",
+                "requestChannel": "APP",
+                "userId": removeThePlus(phoneNumber),
+                "partnerId": "LOOP",
+                "productSet": "LOOP"
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Bearer': process.env.LOOP_TOKEN
+                }
+            }
+        ).then(data => {
+            console.log(data);
+            response = `
+            CON 
+            Welcome ${data.firstName} What would you like to check?
+            1. Account Balance
+            2. KYC Status
+            3. Check Loan Limit`;
+        }).catch(error => {
+            console.error();
+            response = `
+            END You are not a registered customer
+            `;
+        });
+    } else if (text == "1") {
+        // Fetch account balance from the wallet
+        let balance;
+        response = `END Your account balance is ${balance}`;
+    } else if (text == "2") {
+        // Run through the customer KYC check API and response with the status
+        let kycStatus;
+        response = `END Your KYC status is ${kycStatus}`;
+    } else if (text == "3") {
+        // Check loan limit for a user who has a loan limit
+        // Check loan query API
+        let limit;
+        response `END Your loan limit is ${limit}`;
+    } else {
+        response = `END You have selected the wrong input`;
     }
 
     res.set("Content-Type: text/plain");
